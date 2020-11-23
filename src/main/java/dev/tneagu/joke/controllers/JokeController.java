ca67f6980d256db4a838460b655ae8094761cad8
Tiberiu Neagu <tiberiu-mihai.neagu@open-groupe.com>
2020-11-23 18:21:22 +0200

    added mvc files

---
+++ src/main/java/dev/tneagu/joke/controllers/JokeController.java
@@ -0,0 +1,27 @@
+package dev.tneagu.joke.controllers;
+
+
+import dev.tneagu.joke.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model){
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "chucknorris";
+    }
+}
